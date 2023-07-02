ea68bcd1b1ee0e59f8be4d642f045466c5771fe1
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-07-02 23:05:15 +0200

    feat:add PickNMix project

---
+++ PickNMix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace PickNMix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stuffs=new ArrayList();
+            stuffs.Add(1);
+            stuffs.Add("car");
+            stuffs.Add(true);
+            stuffs.Add(23d);
+
+            foreach(var stuff in stuffs)
+            {
+                Console.WriteLine(stuff);
+            }
+        }
+    }
+}
